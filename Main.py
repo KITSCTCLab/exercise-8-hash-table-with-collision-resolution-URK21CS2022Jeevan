1036a8d72aa6fea75529bb563629480de59120db
URK21CS2022Jeevan <100009502+URK21CS2022Jeevan@users.noreply.github.com>
2022-10-12 09:12:26 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,29 +1,85 @@
-import re
+[9:11 am, 12/10/2022] Prince Joshua Macha: from typing import List
 
-def display_hash(hashtable) -> None:
-    for element in range(len(hashtable)):
-        print(f"{element}", end=" ")
-        values = hashtable[element]
-        for value in values:
-            print(f"--> {value}", end=" ")
-        print()
+def merge_sort(myList) -> None:
+  if len(myList) > 1:
+        mid = len(myList) // 2
+        left = myList[:mid]
+        right = myList[mid:]
 
-def Hashing(keyvalue) -> int:
-	return keyvalue % len(HashTable)
+        # Recursive call on each half
+        merge_sort(left)
+        merge_sort(right)
 
-def insert(Hashtable, keyvalue, value) -> None:
-	Hashtable[Hashing(keyvalue)].append(value)
+        # Two iterators for traversing the two halves
+        i = 0
+        j = 0
+        
+        # Iterator for the main list
+        k = 0
+        
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+              # The value from the left half has been used
+              myList[k] = left[i]
+              # Move the iterator forward
+              i += 1
+            else:
+                myList[k] = right[j]
+                j += 1
+            # Move to the next slot
+            k += 1
 
-# Do not edit the following code
-hash_table_size = int(input())
-# Create Hashtable as a list of list.
-HashTable = [[] for _ in range(hash_table_size)]
+        # For all the remaining values
+        while i < len(left):
+            myList[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            myList[k]=right[j]
+            j += 1
+            k += 1
+
+
+# Do not change the following code
 input_data = input()
 data = []
-for item in re.split('], |].', input_data):
-  item = item[1:]
-  data = item.split(', ')
-  if len(data) > 1:
-    insert(HashTable, int(data[0]), data[1])
+for item in input_data.split(', '):
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+merge_sort(data)
+print(data)
+[9:12 am, 12/10/2022] Prince Joshua Macha: from typing import List
 
-display_hash (HashTable)
+# divide function
+def partition(arr,low,high):
+   i = ( low-1 )
+   pivot = arr[high] # pivot element
+   for j in range(low , high):
+      # If current element is smaller
+      if arr[j] <= pivot:
+         # increment
+         i = i+1
+         arr[i],arr[j] = arr[j],arr[i]
+   arr[i+1],arr[high] = arr[high],arr[i+1]
+   return ( i+1 )
+# sort
+def quick_sort(arr,low,high):
+   if low < high:
+      # index
+      pi = partition(arr,low,high)
+      # sort the partitions
+      quick_sort(arr, low, pi-1)
+      quick_sort(arr, pi+1, high)
+   return arr
+
+input_data = input()
+data = []
+for item in input_data.split(', '):
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+print(quick_sort(data, 0, len(data)-1))
